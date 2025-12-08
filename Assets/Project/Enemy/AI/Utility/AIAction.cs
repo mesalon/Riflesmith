6f2b526dbe85074da16f1b1f6588e99abefdf701
mesalon <mesalondwas@gmail.com>
2025-12-08 15:27:56 -0500

    update

--- Assets/Project/Enemy/AI/Utility/AIAction.cs
+++ Assets/Project/Enemy/AI/Utility/AIAction.cs
@@ -1,4 +1,5 @@
 [System.Serializable] public abstract class AIAction {
 	public abstract float GetScore();
-	public abstract void Execute();
+	public abstract void ();
+	public abstract void Tick();
 }
--- Assets/Project/Enemy/AI/Utility/UtilityAI.cs
+++ Assets/Project/Enemy/AI/Utility/UtilityAI.cs
@@ -17,24 +17,19 @@ public class UtilityAI {
 	public void Tick() {
 		actions.Sort((a, b) => b.GetScore().CompareTo(a.GetScore()));
 
-		var top = new (float score, AIAction action)[3];
-		for(int i=0; i<3; i++) top[i] = (float.MinValue, null);
+		float highest = 0;
+		AIAction action = null;
 		foreach (AIAction a in actions) {
-			float v = a.GetScore();
-			if (v > top[0].score) {
-				top[2] = top[1]; 
-				top[1] = top[0]; 
-				top[0] = (v, a);
-			}
-			else if (v > top[1].score) {
-				top[2] = top[1]; 
-				top[1] = (v, a);
-			}
-			else if (v > top[2].score) {
-				top[2] = (v, a);
+			float score = a.GetScore();
+			if (score > highest) {
+				highest = score;
+				action = a;
 			}
 		}
-		Ext.Label(ctx.transform.position + 2 * Vector3.up, $"{top[0].action} ({top[0].score})\n{top[1].action} ({top[1].score})\n{top[2].action} ({top[2].score})");
-		top[0].action.Execute();
+		if (action != current) {
+			current = action;
+			current.Enter();
+		}
+		Ext.Label(ctx.transform.position + 2 * Vector3.up, $"Highest: {current} ({current.GetScore()})");
 	}
 }
