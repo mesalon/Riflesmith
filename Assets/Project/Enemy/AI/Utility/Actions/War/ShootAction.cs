c6d8a428ff92f47058b4186aa37066bf367caa8e
mesalon <mesalondwas@gmail.com>
2025-12-27 03:49:36 -0500

    save

--- Assets/Project/Enemy/AI/Utility/Actions/War/ShootAction.cs
+++ Assets/Project/Enemy/AI/Utility/Actions/War/ShootAction.cs
@@ -1,23 +1,10 @@
 using UnityEngine;
 
 public class ShootAction : AIAction {
+	private Blackboard board => ctx.board;
 	private readonly Enemy ctx;
-	private bool InCover {
-		get {
-			Vector3? cover = Cover;
-			if (cover == null) return false;
-			return (cover.Value - ctx.transform.position).sqrMagnitude < 1;
-		}
-	}
-	private Vector3? Cover {
-		get {
-			if (ctx.blackboard.cover != null && ctx.blackboard.cover.GetBestPoint(1, 1, out CoverTask task)) {
-				return task.cover.position;
-			}
-			return null;
-		}
-	}
-	private bool peeking;
+	private bool combatRoutine;
+	private bool isPeeking;
 	float t;
 
 	public ShootAction(Enemy ctx) {
@@ -25,45 +12,40 @@ public class ShootAction : AIAction {
 	}
 
 	public override float GetScore() {
-		return (ctx.blackboard.targetLKP == null ? 0 : 1) * 1 - ctx.blackboard.LKPAge * 0.05f;
+		return (board.targetLKP == null ? 0 : 0.8f) * 1 - board.LKPAge * 0.05f;
 	}
 
 	public override void Enter() {
 		ctx.locomotion.Stop();
-		ctx.blackboard.cover = new(ctx.transform.position, ctx.blackboard.targetLKP.Value, ctx.blackboard.seeker, CoverParams.Default);
+		board.cover = new(ctx.transform.position, board.targetLKP.Value, board.seeker, CoverParams.Default);
 	}
 
 	public override void Tick() {
-		ctx.blackboard.cover?.ShowDebug();
-		Vector3? cover = Cover;
-		Ext.Label(ctx.transform.position, $"{(cover != null ? (cover.Value - ctx.transform.position).sqrMagnitude : "No cover")}");
-		if (inCover) {
-			if (peeking) {
-				ctx.handling.FireAt(ctx.blackboard.targetLKP.Value);
-				if (t >= 4) {
-					t = 0;
-					peeking = false;
-					ctx.locomotion.Move(, Pace.Walk);
+		if (board.cover.TryGetCover(out CoverTask cover)) {
+			if (!combatRoutine) {
+				ctx.locomotion.Move(cover.point.position, Pace.Jog);
+				if ((cover.point.position - ctx.transform.position).sqrMagnitude < 0.01f) {
+					combatRoutine = true;
+					t = 3;
 				}
 			} else {
+				if (isPeeking) { ctx.handling.FireAt(board.targetLKP.Value); }
 				if (t >= 4) {
-					ctx.locomotion.Move(cover + ctx.transform.rotation * Vector3.right * 0.4f, Pace.Walk);
-					peeking = true;
+					if (isPeeking) {
+						ctx.locomotion.Move(cover.point.position, Pace.Walk);
+						board.expectsToSeeTarget = false;
+						isPeeking = false;
+					} else {
+						ctx.locomotion.Move(cover.ReturnFirePoint, Pace.Walk);
+						board.expectsToSeeTarget = true;
+						isPeeking = true;
+					}
 					t = 0;
 				}
+				t += Time.deltaTime;
 			}
-			t += Time.deltaTime;
 		} else {
-			if (ctx.blackboard.cover != null) {
-				query.FindCover();
-				if (query.GetBestPoint(1, 1, out cover)) {
-					ctx.locomotion.Move(cover, Pace.Run);
-					query = null;
-				}
-			} else if (ctx.locomotion.Arrived) { 
-				inCover = true; 
-				t = 3;
-			}
+			board.cover.Search();
 		}
 	}
 
