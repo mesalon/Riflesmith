bded7bae7a09961c188197ac17eb8755d0de55f6
mesalon <mesalondwas@gmail.com>
2026-01-23 01:04:43 -0500

    save

--- Assets/Editor/PrefabReplacer.cs
+++ Assets/Editor/PrefabReplacer.cs
@@ -1,32 +1,37 @@
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEditor;
+using System.Collections.Generic;
 
-public class PrefabReplacer : EditorWindow {
-	[SerializeField] List<GameObject> prefabs;
-	[SerializeField] SerializedObject sObject;
-	[SerializeField] float sus;
-	[SerializeField] private VisualTreeAsset tree;
+public class SimpleSwapper : EditorWindow {
+    GameObject target, replacement;
 
-	[MenuItem("Tools/PrefabReplacer")]
-	public static void ShowExample() {
-		GetWindow<PrefabReplacer>().titleContent = new("PrefabReplacer");
-	}
+    [MenuItem("Tools/Simple Swapper")]
+    static void Init() => GetWindow<SimpleSwapper>().Show();
 
-	private void OnEnable() { sObject = new(this); }
-	private void OnDisable() { sObject?.Dispose(); }
+    void OnGUI() {
+        target = (GameObject)EditorGUILayout.ObjectField("Replace", target, typeof(GameObject), false);
+        replacement = (GameObject)EditorGUILayout.ObjectField("With", replacement, typeof(GameObject), false);
 
-	public void CreateGUI() {
-		tree.CloneTree(rootVisualElement);
-		rootVisualElement.Bind(sObject);
-		rootVisualElement.Q<Button>("Replace").clicked += () => {
-			foreach (Transform t in Selection.transforms) {
-				GameObject go = PrefabUtility.InstantiatePrefab(prefabs[Random.Range(0, prefabs.Count)]) as GameObject;
-				go.transform.SetPose(t.position, t.rotation);
-				DestroyImmediate(t.gameObject);
-			}
-		};
-	}
+        if (GUILayout.Button("Swap All") && target && replacement) {
+            Swap();
+        }
+    }
+
+    void Swap() {
+        var objectsToSwap = new List<GameObject>();
+
+        foreach (var go in FindObjectsByType<GameObject>(FindObjectsSortMode.None)) {
+            if (PrefabUtility.GetCorrespondingObjectFromSource(go) == target)
+                objectsToSwap.Add(go);
+        }
+
+        foreach (var go in objectsToSwap) {
+            if (go == null) continue;
+            GameObject newObj = (GameObject)PrefabUtility.InstantiatePrefab(replacement, go.transform.parent);
+            Undo.RegisterCreatedObjectUndo(newObj, "Swap Prefab");
+            newObj.transform.SetPositionAndRotation(go.transform.position, go.transform.rotation);
+            newObj.transform.localScale = go.transform.localScale;
+            Undo.DestroyObjectImmediate(go);
+        }
+    }
 }
